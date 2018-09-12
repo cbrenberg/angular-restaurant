e74cde72d71d41174f4913f5af56a85869a0b0de
Christopher Brenberg <cbrenberg@gmail.com>
2018-09-12 14:18:36 -0500

    allow users to add new foods

--- client.js
+++ client.js
@@ -9,4 +9,11 @@ foodApp.controller('FoodController', function() {
     {name: 'taco'},
     {name: 'potato'}
   ];
+
+  self.foodToAdd = '';
+
+  self.addFood = function(userInput) {
+    self.foods.push({ name: userInput });
+    self.foodToAdd = '';
+  };
 })
