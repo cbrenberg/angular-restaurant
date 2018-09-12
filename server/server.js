f4518cfbc15641a9051a85ccb46a1665dbb9bc2e
Christopher Brenberg <cbrenberg@gmail.com>
2018-09-12 14:26:46 -0500

    run app on node express server

---
+++ server/server.js
@@ -0,0 +1,17 @@
+//requires
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+
+//uses
+app.use(express.static('server/public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+//globals
+const PORT = 5000;
+
+//spin up server
+app.listen(PORT, () => {
+  console.log('Listening on port:', PORT);
+});
